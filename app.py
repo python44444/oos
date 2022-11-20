ebdbc21e7550578265866f0f7a26b6c002e8acbb
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-11-20 13:47:07 +0900

    update]

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    return redirect("/login")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
