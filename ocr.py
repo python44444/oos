a3836cc55451a9ea1361324aebc9c3f674d56d57
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-11-26 18:02:26 +0900

    update

--- ocr.py
+++ ocr.py
@@ -8,29 +8,40 @@ tool = tools[0]
 photo = "static/images/image.jpeg"
 
 
-def ocr(photo):
-    img = Image.open(photo)
-
-    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # 2値化（100:２値化の閾値／画像を見て調整する）
-    ret, thresh1 = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY)
-    # ノイズ処理（モルフォロジー変換）
-    kernel = np.ones((5, 5), np.uint8)
-    img_after = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
-
-    new_image = img_after.copy()
-    if new_image.ndim == 2:
+def cv2pil(image):
+    """OpenCV型 -> PIL型"""
+    new_image = image.copy()
+    if new_image.ndim == 2:  # モノクロ
         pass
     elif new_image.shape[2] == 3:  # カラー
-        new_image = cv2.cvtColor(img_after, cv2.COLOR_BGR2RGB)
+        new_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     elif new_image.shape[2] == 4:  # 透過
-        new_image = cv2.cvtColor(img_after, cv2.COLOR_BGRA2RGBA)
+        new_image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGBA)
     new_image = Image.fromarray(new_image)
+    return new_image
+
+
+def ocr(photo):
+    img = cv2.imread(photo)
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    cv2.imwrite("static/images/gray.jpeg", img_gray)
+
+    img_reverse = 255 - img_gray
+    cv2.imwrite("static/image/reverse.jpeg", img_reverse)
+
+    # 2値化（100:２値化の閾値／画像を見て調整する）
+    ret, thresh1 = cv2.threshold(img_gray, 110, 255, cv2.THRESH_BINARY)
+    # ノイズ処理（モルフォロジー変換
+    kernel = np.ones((2, 7), np.uint8)
+    img_opening = cv2.dilate(thresh1, kernel)
+    # img_opening = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
+
+    image = cv2pil(img_opening)
+    image.save("static/images/pillow.jpeg")
 
     txt = tool.image_to_string(
-        new_image,
-        lang="jpn",
-        builder=pyocr.builders.DigitBuilder(tesseract_layout=6)
+        image, lang="jpn", builder=pyocr.builders.DigitBuilder(tesseract_layout=6)
     )
     return txt
 
